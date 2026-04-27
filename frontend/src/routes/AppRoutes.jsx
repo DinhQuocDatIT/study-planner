0f81a28bfe70064946a01e1d1f17fe621b62c656
DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>
2026-04-27 13:57:55 +0700

    create main layout structure

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import { Login, Register } from "../pages/auth";
+import MainLayout from "../layouts/MainLayout/MainLayout";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
